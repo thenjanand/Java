6a47a2d9cbc4046b254a0eb5262f1b3f75c56df4
NEERAJ ANAND <njanand.xenon@gmail.com>
2022-10-15 22:53:04 +0530

    Area of circle

---
+++ areaOfCircle.java
@@ -0,0 +1,13 @@
+/*Area of circle */
+import java.util.Scanner;
+
+public class areaOfCircle {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    float radius  = sc.nextFloat();
+    float area=3.14f * radius * radius;
+    System.out.println(area);
+
+  }
+  
+}
